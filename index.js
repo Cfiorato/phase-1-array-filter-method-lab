e0cfa25c2af64df2cd69cbecc32bdd2f21107221
Cameron Fiorato <cameronclayfiorato@gmail.com>
2021-11-25 15:52:40 -0600

    commit tree e31bfe10603b4ea0d99c7502b4407c3d4215086d

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(array, string) {
+    let nameMatch = array.filter(function(driver){
+        return driver.toLowerCase() === string.toLowerCase()
+    });
+    return nameMatch
+};
+
+function fuzzyMatch(array, string){
+    let letterMatch = array.filter(function(driver){
+        return driver.startsWith(string)
+    })
+    return letterMatch
+}
+
+function matchName(array, string){
+    let match = array.filter(function(driver){
+        return driver.name === string || driver.hometown === string
+    })
+    return match
+}
